4537f65f8bcc19636a75583b5051e845077248d7
taiseidayo <tkapple0827aaa@gmail.com>
2022-04-07 03:41:26 +0900

    Initial Commit

--- components/Editor/index.ts
+++ components/Editor/index.ts
@@ -1,4 +1,4 @@
-import { Editor as DraftEditor, EditorState } from "draft-js";
+import { Editor as DraftEditor, EditorState, RichUtils } from "draft-js";
 import "draft-js/dist/Draft.css";
 import { useState } from "react";
 
@@ -7,7 +7,26 @@ const Editor = () => {
     EditorState.createEmpty()
   );
 
-  return <DraftEditor editorState={editorState} onChange={setEditorState} />;
+  const handleBold = () => {
+    setEditorState(RichUtils.toggleInlineStyle(editorState, "BOLD"));
+  };
+
+  return (
+    <div className="m-4 max-w-xl">
+      <div className="mb-1 pr-4 flex items-center">
+        <div className="ml-auto flex items-center space-x-5">
+          <button onClick={handleBold}>B</button>
+        </div>
+      </div>
+      <div className="shadow-sm border border-gray-300 rounded-md sm:text-sm overflow-scroll h-[500px] p-3 prose prose-stone">
+        <DraftEditor
+          editorState={editorState}
+          onChange={setEditorState}
+          placeholder="Tell a story..."
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Editor;
